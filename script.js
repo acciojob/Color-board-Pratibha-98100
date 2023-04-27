076a715b9c64a6523c9b46893d291b0e2934a9c7
Pratibha-98100 <130212205+Pratibha-98100@users.noreply.github.com>
2023-04-28 01:04:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+
+// Get all the squares
+const squares = document.querySelectorAll('.square');
+
+// Loop through all the squares
+squares.forEach(square => {
+  // Add event listener for mouseenter
+  square.addEventListener('mouseenter', () => {
+    // Change the background color to green
+    square.style.backgroundColor = 'green';
+    
+    // Remove the background color after 1 second
+    setTimeout(() => {
+      square.style.backgroundColor = '';
+    }, 1000);
+  });
+});
+
+
+
+
+
